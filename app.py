a62927c257aca755de43723796fe8f576a1a4d13
ZumbiDoPython <pepel.smourao@gmail.com>
2022-05-17 19:41:31 -0300

    vai de priemira 4

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import os
+import json
+import uuid
+from urllib import request, parse
+from urllib.error import URLError, HTTPError
 from flask import Flask, jsonify, request
 
 #LINK PARA ENTENDER O ENVIO DE NOTIFICAÇÃO POR API: https://help.blip.ai/hc/pt-br/articles/4474382664855-Como-enviar-notificações-WhatsApp-via-API-do-Blip
